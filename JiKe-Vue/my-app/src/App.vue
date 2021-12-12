f38953c47e1ef509a8139428a1f54e9af5b7d624
HFwas <1558934404@qq.com>
2021-12-12 13:02:22 +0800

    "fix://修改为VUE代码"

---
+++ JiKe-Vue/my-app/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">hello world, {{msg}}
+    <div>
+      <input type="text" v-model="info">
+      <button @click="hanleClick">输入</button>
+        <ToDoItem v-for="item in list" :key="item" :item="item">{{item}}</ToDoItem>
+    </div>
+  </div>
+</template>
+
+<script>
+import ToDoItem from './components/ToDoItem.vue'
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      msg: 'hello,getttime',
+      info: '',
+      list: []
+    }
+  },
+  methods: {
+    hanleClick(){
+      // console.log(this.info)
+      this.list.push(this.info)
+    }
+  },
+  components: {
+    ToDoItem,
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ JiKe-Vue/my-app/src/components/ToDoItem.vue
@@ -0,0 +1,15 @@
+<template>
+  <li class="item">{{item}}</li>
+</template>
+
+<script>
+export default {
+  props: ['item'],
+}
+</script>
+
+<style scoped>
+  .item {
+    color: red;
+  }
+</style>
