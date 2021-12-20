0c74e9e31bf5af1db13f46aab423816f08f8ca01
HFwas <1558934404@qq.com>
2021-12-20 21:45:08 +0800

    "feat://ToDoList自定义事件"

---
+++ vue_cli/11_src_ToDoList自定义事件/main.js
@@ -0,0 +1,15 @@
+// 改文件是整个项目的的入口文件
+// 引入VUE
+import Vue from 'vue'
+// 引入APP组件，他是所有组件的父组件
+import App from './App.vue'
+
+// 关闭VUE的生产提示
+Vue.config.productionTip = false
+
+// 创建VUE实例对象 --- vm
+new Vue({
+  el:'#app',
+  // 下面这行代码一会解释，完成了这个功能，将APP组件放入容器当中
+  render: h => h(App),
+})
