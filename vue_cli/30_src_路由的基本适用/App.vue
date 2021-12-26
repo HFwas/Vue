de3928ec8ffc91965a8903c4b4b0b9b9b7c2ff76
HFwas <1558934404@qq.com>
2021-12-26 20:19:47 +0800

    "feat://路由的基本适用"

---
+++ vue_cli/30_src_路由的基本适用/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <div className="row">
+      <div className="col-xs-offset-2 col-xs-8">
+        <div className="page-header"><h2>Vue Router Demo</h2></div>
+      </div>
+    </div>
+    <div className="row">
+      <div className="col-xs-2 col-xs-offset-2">
+        <div className="list-group">
+          <!-- 原始html中我们使用a标签实现页面的跳转 -->
+          <!-- <a class="list-group-item active" href="./about.html">About</a> -->
+          <!-- <a class="list-group-item" href="./home.html">Home</a> -->
+
+          <!-- Vue中借助router-link标签实现路由的切换 -->
+          <router-link className="list-group-item" active-class="active" to="/about">About</router-link>
+          <router-link className="list-group-item" active-class="active" to="/home">Home</router-link>
+        </div>
+      </div>
+      <div className="col-xs-6">
+        <div className="panel">
+          <div className="panel-body">
+            <!-- 指定组件的呈现位置 -->
+            <router-view></router-view>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+}
+</script>
