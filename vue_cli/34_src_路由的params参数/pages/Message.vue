99807073885de79fbc7c9397bb61c34c482ae78f
HFwas <1558934404@qq.com>
2021-12-27 00:02:29 +0800

    "feat://路由的params参数"

---
+++ vue_cli/34_src_路由的params参数/pages/Message.vue
@@ -0,0 +1,36 @@
+<template>
+	<div>
+		<ul>
+			<li v-for="item in messageList" :key="item.id">
+
+        <!-- 跳转路由并携带query参数，to的对象写法 -->
+        <router-link :to="{
+          name:'xiangqing',
+          // path:'/home/message/detail',
+          params:{
+            id:item.id,
+            title:item.title
+          }
+				}">
+          {{item.title}}
+        </router-link>&nbsp;&nbsp;
+			</li>
+		</ul>
+    <router-view></router-view>
+	</div>
+</template>
+
+<script>
+	export default {
+		name:'Message',
+    data(){
+      return{
+        messageList:[
+          {id:'001',title:'消息001'},
+          {id:'002',title:'消息002'},
+          {id:'003',title:'消息003'}
+        ]
+      }
+    }
+	}
+</script>
