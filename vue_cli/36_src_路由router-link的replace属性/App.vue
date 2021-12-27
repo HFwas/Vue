e2e816205908602c0065cf3dce4b80c041cb2201
HFwas <1558934404@qq.com>
2021-12-27 23:06:15 +0800

    "feat://路由router-link的replace属性"

---
+++ vue_cli/36_src_路由router-link的replace属性/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <div className="row">
+      <Banner/>
+    </div>
+    <div className="row">
+      <div className="col-xs-2 col-xs-offset-2">
+        <div className="list-group">
+          <!-- 原始html中我们使用a标签实现页面的跳转 -->
+          <!-- <a class="list-group-item active" href="./about.html">About</a> -->
+          <!-- <a class="list-group-item" href="./home.html">Home</a> -->
+
+          <!-- Vue中借助router-link标签实现路由的切换 -->
+          <router-link replace className="list-group-item" active-class="active" to="/about">About</router-link>
+          <hr/>
+          <router-link replace className="list-group-item" active-class="active" to="/home">Home</router-link>
+        </div>
+      </div>
+      <div className="col-xs-6">
+        <div className="panel">
+          <div className="panel-body">
+            <!-- 指定组件的呈现位置 -->
+            <router-view></router-view>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import Banner from "@/components/Banner";
+  export default {
+    name: 'App',
+    components: {Banner},
+  }
+</script>
---
+++ vue_cli/36_src_路由router-link的replace属性/components/Banner.vue
@@ -0,0 +1,15 @@
+<template>
+  <div className="col-xs-offset-2 col-xs-8">
+    <div className="page-header"><h2>Vue Router Demo</h2></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Banner"
+}
+</script>
+
+<style scoped>
+
+</style>
