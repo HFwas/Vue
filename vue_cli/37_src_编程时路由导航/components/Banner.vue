4ed12fa09e14df9ac61ba0a764cefcc64585cb6d
HFwas <1558934404@qq.com>
2021-12-28 21:57:56 +0800

    "feat://编程时路由导航"

---
+++ vue_cli/37_src_编程时路由导航/components/Banner.vue
@@ -0,0 +1,29 @@
+<template>
+  <div className="col-xs-offset-2 col-xs-8">
+    <div className="page-header"><h2>Vue Router Demo</h2></div>
+    <button @click="back">后退</button>
+    <button @click="forward">前进</button>
+    <button @click="testGo">go</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Banner",
+  methods:{
+    back(){
+      this.$router.back();
+    },
+    forward(){
+      this.$router.forward();
+    },
+    testGo(){
+      this.$router.go(2);
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
