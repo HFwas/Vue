1a53b912c3646b4bdd866628ec7c63cedcd8df7a
HFwas <1558934404@qq.com>
2022-01-02 21:23:38 +0800

    "feat://三级联动使用动态数据"

--- app/src/store/home/index.js
+++ app/src/store/home/index.js
@@ -1,7 +1,25 @@
 //home模块的仓库
-const state = {};
-const mutations = {};
-const actions = {};
+import {reqCategoryList} from "@/api";
+
+const state = {
+    // state中数据默认初始值别瞎写，服务器返回对象，服务器返回数组，【根据接口返回值初始化的】
+    categoryList:[]
+};
+const mutations = {
+    CATEGORYLIST(state, categoryList){
+        state.categoryList = categoryList;
+    }
+
+};
+const actions = {
+    // 通过API里面的接口函数调用，向服务器发请求，获取服务器的数据
+    async categoryList(commit){
+        let categoryList = await reqCategoryList();
+        if (categoryList.code === 200) {
+            commit.commit('CATEGORYLIST', categoryList.data);
+        }
+    }
+};
 const getters = {};
 
 export default {
