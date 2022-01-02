87e4c865434e063dd64d89ea26f1da86ac274007
HFwas <1558934404@qq.com>
2022-01-02 17:39:48 +0800

    "feat://首页静态页面的搭建"

---
+++ app/src/api/request.js
@@ -0,0 +1,12 @@
+//对于axios进行二次封装
+import axios from "axios";
+
+// 1.
+
+
+
+
+
+
+// 对外暴露
+export default axios;
--- app/src/main.js
+++ app/src/main.js
@@ -2,7 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 
 Vue.config.productionTip = false
-
+// 三级联动组件---全局组件
+import TyprNav from '@/pages/Home/TypeNav';
+// 第一个参数：全局组件的名字 第二个参数：哪一个组件
+Vue.component(TyprNav.name, TyprNav);
 // 引入路由
 import router from '@/router';
 new Vue({
