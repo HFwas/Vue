821776ea71c22719190c658b46f1f697f33acd8b
HFwas <1558934404@qq.com>
2022-01-06 22:26:38 +0800

    "feat://search模块"

--- app/src/api/index.js
+++ app/src/api/index.js
@@ -17,3 +17,13 @@ export const reqGetBannerList = () => mockRequests.get('/banner');
 
 // 获取floor数据
 export const reqFloorList = ()=> mockRequests.get('/floor');
+
+// 获取搜索模块数据，地址：/api/list   参数：需要携带参数
+// 当前这个接口（获取搜索模块的数据），给服务器传递一个默认参数【至少是一个空对象】
+export const reqGetSearchInfo = (params) => requests({
+    url:'/list',
+    method:'POST',
+    data:{
+        params
+    }
+})
--- app/src/store/search/index.js
+++ app/src/store/search/index.js
@@ -1,7 +1,25 @@
+import {reqGetSearchInfo} from "@/api";
 //search模块的仓库
-const state = {};
-const mutations = {};
-const actions = {};
+const state = {
+    searchList:[]
+};
+const mutations = {
+    GETSEARCHINFO(state, searchList) {
+        state.searchList = searchList;
+    }
+};
+const actions = {
+    // 获取search模块数据
+    async getSearchInfo({commit}, params={}) {
+        // 当前这个reqGetSearchInfo这个函数在调用获取服务器数据的时候，至少传递一个参数（空对象）
+        // params行参数：是当用户派发action的时候，第二个参数传递过来的，至少是一个空对象
+        let response = await reqGetSearchInfo(params);
+        if (response.code === 200) {
+            commit.commit('GETSEARCHINFO', response.data);
+        }
+    }
+};
+//计算属性，在项目当中是为了简化数据而生
 const getters = {};
 
 export default {
